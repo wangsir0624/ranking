cdeca630a766dd77885d3f5eda22e74a204a37a5
wangsir0624 <1636801376@qq.com>
2018-01-28 23:51:28 +0800

    fix

--- tests/RankingTestBase.php
+++ tests/RankingTestBase.php
@@ -19,7 +19,6 @@ class RankingTestBase extends TestCase
             'schema' => 'tcp',
             'host' => '127.0.0.1',
             'port' => 6379,
-            'password' => '',
             'database' => 0
         ]);
 
