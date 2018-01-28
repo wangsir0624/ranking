1e90c0c8bda36a8ca62b9699759ac79063fe072d
wangjian <1636801376@qq.com>
2018-01-28 23:44:19 +0800

    fix

--- tests/RankingTestBase.php
+++ tests/RankingTestBase.php
@@ -18,8 +18,8 @@ class RankingTestBase extends TestCase
         $client = new Client([
             'schema' => 'tcp',
             'host' => '127.0.0.1',
-            'port' => 6380,
-            'password' => 'ranking',
+            'port' => 6379,
+            'password' => '',
             'database' => 0
         ]);
 
